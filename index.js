6a8fff3461404a7b1a5a7b6e1b7c267d3697fc43
Irvin Dinkins <irvdude44@gmail.com>
2023-05-04 22:19:42 -0400

    passed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+// const data = {
+//     name: "irvin",
+//     email: "irvin@email.com"
+// }
+
+// const objectConfig = 
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(function (res) {
+        return res.json()
+    })
+    .then(function (object) {
+        document.body.textContent = object.id
+    })
+    .catch(function (error) {
+        document.body.textContent = error.message
+    })
+
+}
